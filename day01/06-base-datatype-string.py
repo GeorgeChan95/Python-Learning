46e94cc2a4a5e257aebff187be1b70914f10cf3f
George <george_95@126.com>
2025-11-04 15:05:34 +0800

    基本数据类型 string

--- day01/06-base-datatype-string.py
+++ day01/06-base-datatype-string.py
@@ -21,3 +21,21 @@ print(type(flag))
 
 print(f"flag的类型是：{type(name)}") # flag的类型是：<class 'str'>
 print(f"name的类型是：{type('George')}") # name的类型是：<class 'str'>
+
+
+# Python 仅保存一份相同且不可变字符串，不同的值被存放在字符串的驻留池中，Python 的
+# 驻留机制对相同的字符串只保存一份拷贝，后续创建相同字符串时，不会开辟新空间，而是把该字
+# 符串的地址赋给新创建的变量。
+# 字符串驻留机制
+str1 = "hello"
+str2 = "hello"
+str3 = "hello"
+# id()函数是可以返回对象/数据的内存地址
+print("str1的地址：", id(str1)) # str1的地址： 2501456790368
+print("str2的地址：", id(str2)) # str2的地址： 2501456790368
+print("str3的地址：", id(str3)) # str3的地址： 2501456790368
+
+str1 = "111"
+print("str1的地址：", id(str1)) # str1的地址： 1836574082848
+print("str2的地址：", id(str2)) # str2的地址： 2501456790368
+print("str3的地址：", id(str3)) # str3的地址： 2501456790368
