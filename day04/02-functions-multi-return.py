722a2c5535c5a1cc1152c0e97586caa06da17522
George <george_95@126.com>
2025-11-14 16:53:58 +0800

    调用函数返回多个结果

--- day04/02-functions-multi-return.py
+++ day04/02-functions-multi-return.py
@@ -10,3 +10,13 @@ def f1(num1, num2):
 
 result = f1(2, 5)
 print(f"调用函数返回多个结果，结果1={result[0]}, 结果2={result[1]}")
+
+print("---------------------------------------------------")
+
+def f2(param1, param2):
+    return param1 + param2, param2 - param1
+
+
+# 使用多个参数
+r1, r2 = f2(1, 9)
+print(f"r1={r1} \t r2={r2}")
