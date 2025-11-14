2b72dcf4ec02ded0de51940f36b4187885c18ef0
George <george_95@126.com>
2025-11-14 16:43:36 +0800

    自定义函数

---
+++ day04/01-functions.py
@@ -0,0 +1,14 @@
+
+
+def get_sum(num1, num2):
+    '''
+    :param num1: 参数1
+    :param num2: 参数2
+    :return: 返回参数1 与 参数2的累加结果
+    '''
+    return num1 + num2
+
+# 调用函数
+sum = get_sum(10, 30)
+print(f"两数相机的结果：{sum}")
+
