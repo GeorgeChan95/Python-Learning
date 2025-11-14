7967d57058dfd780f6e67de1e6205f024fee10f5
George <george_95@126.com>
2025-11-14 19:52:12 +0800

    函数默认参数和不定长参数

--- day04/03-default-multi-params.py
+++ day04/03-default-multi-params.py
@@ -29,3 +29,22 @@ num=2
 num=3
 num=4
 '''
+
+
+# 接收一个人的信息，但是有哪些信息是不确定的，就可以使用关键字可变参数(**kwargs)
+def fun3(**kwargs):
+    print(f"kwargs: {kwargs} 类型：{type(kwargs)}")
+    for kwargs_name in kwargs:
+        print(f"参数名：{kwargs_name} 参数值：{kwargs[kwargs_name]}")
+
+fun3(name="george", age=18, email="george@qq.com")
+# fun3(name="hsp", age=18, email="hsp@qq.com", sex="男", address="北京")
+fun3()
+'''
+打印如下：
+kwargs: {'name': 'george', 'age': 18, 'email': 'george@qq.com'} 类型：<class 'dict'>
+参数名：name 参数值：george
+参数名：age 参数值：18
+参数名：email 参数值：george@qq.com
+kwargs: {} 类型：<class 'dict'>
+'''
