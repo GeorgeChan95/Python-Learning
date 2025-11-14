c190fadc8c82b99776257427e763dfaca8b2e87f
George <george_95@126.com>
2025-11-14 20:07:26 +0800

    Python 调用另一个.py 文件的函数

---
+++ day04/04-another-file-fun.py
@@ -0,0 +1,6 @@
+# 调用另一个py文件的函数
+
+import f1
+
+result = f1.get_sum(2, 4)
+print(result)
---
+++ day04/f1.py
@@ -0,0 +1,7 @@
+def get_sum(num1, num2):
+    '''
+    :param num1: 参数1
+    :param num2: 参数2
+    :return: 返回参数1 与 参数2的累加结果
+    '''
+    return num1 + num2
