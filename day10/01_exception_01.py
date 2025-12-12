3f4964767d5989dd48a60a89d164fa04d476e488
George <george_95@126.com>
2025-12-12 14:37:56 +0800

    常见的Exception

--- day10/01_exception_01.py
+++ day10/01_exception_01.py
@@ -8,10 +8,10 @@ res = None
 try:
     num1 = 10
     num2 = 0
-    # res = num1 / num2
-    res = num2 / num1
+    res = num1 / num2
+    # res = num2 / num1
 except Exception as e:
-    print(f"捕获到异常信息：P{e}")
+    print(f"捕获到异常信息：{e}，异常类型：{type(e)}")
 else:
     print(f"没有发生异常，计算结果：{res}")
 finally:
