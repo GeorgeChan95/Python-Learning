7f231f4a91d9755c47e2bcd36cfcc42e2c8f801c
George <george_95@126.com>
2025-12-20 16:15:35 +0800

    update

--- day11/07_append_line_num.py
+++ day11/07_append_line_num.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2025/12/20 9:12
 # @Author  : George
-# @File    : 001_append_line_num.py
+# @File    : 07_append_line_num.py
 
 # 读取b.txt , 给每一行内容加行号
 
--- day11/08_img_copy.py
+++ day11/08_img_copy.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2025/12/20 9:46
 # @Author  : George
-# @File    : 002_img_copy.py
+# @File    : 08_img_copy.py
 
 # 实现二进制文件内容的拷贝
 with open("d:\\1.png", "rb") as sourceF, open("d:\\11.png", "wb") as targetF:
