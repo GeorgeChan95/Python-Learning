1d7dab49320120924aa455387a8394f64f285627
George <george_95@126.com>
2026-01-24 15:11:55 +0800

    死锁

--- day13_thread/06_deadlock_1.py
+++ day13_thread/06_deadlock_1.py
@@ -11,10 +11,32 @@ from time import sleep
 
 # 死锁
 def fun1():
-    pass
+    lock1.acquire()
+    print("fun1获取了lock1")
+    sleep(2)
+    print("fun1准备获取lock2")
+    flag = lock2.acquire(True, 10)
+    if flag:
+        print("fun1成功获取了lock2")
+        lock2.release()
+        print("fun1释放了lock2")
+    else:
+        print("fun1没有获取lock2")
+    lock1.release()
 
 def fun2():
-    pass
+    lock2.acquire()
+    print("fun2获取了lock2")
+    print("fun2准备获取lock1")
+    flag = lock1.acquire(True, 10) # 获取lock1，最多阻塞10秒，超时失败
+    if flag:
+        print("fun2成功获取了lock1")
+        lock1.release()
+        print("fun2释放了lock1")
+    else:
+        print("fun2没有获取lock1")
+    lock2.release()
+    print("fun2释放了lock2")
 
 
 if __name__ == "__main__":
