b31d385d360077db4b912a18dc819797e4e00e7a
George <george_95@126.com>
2026-02-04 11:15:17 +0800

    多线程实现UDP发送和接收消息

---
+++ day15_tcp&udp/02_thread_udp_receive.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding=utf-8
+# -*- coding: utf-8 -*-
+# @Time    : 2026/2/4 10:54
+# @Author  : George
+# @File    : 02_thread_udp_receive.py
+
+# 使用多线程完成数据的收和发
+from socket import socket, AF_INET, SOCK_DGRAM
+from threading import Thread
+
+def sendData():
+    s = socket(AF_INET, SOCK_DGRAM)
+    # s.bind(('127.0.0.1', 9988))
+    addr = ('127.0.0.1', 8899)
+
+    while True:
+        data = input("请输入内容：")
+        s.sendto(data.encode("utf-8"), addr)
+
+def receiveData():
+    s = socket(AF_INET, SOCK_DGRAM)
+    s.bind(('127.0.0.1', 9988))
+    while True:
+        data = s.recvfrom(1024)
+        print(f"接收到消息内容2：{data[0].decode('utf-8')}, 消息发送发：{data[1]}")
+
+
+
+
+if __name__ == '__main__':
+    t1 = Thread(target=sendData)
+    t2 = Thread(target=receiveData)
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
