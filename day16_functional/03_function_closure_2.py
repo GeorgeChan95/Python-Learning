8368fcd2c2eb9cf7710dacae1e29867befd203b8
George Chan <george_95@126.com>
2026-02-19 21:47:31 +0800

    闭包和自由变量

---
+++ day16_functional/03_function_closure_2.py
@@ -0,0 +1,30 @@
+# Python的闭包和自由变量
+
+# 在 Python 中，如果一个变量：
+# 在函数内部被使用
+# 但它不是这个函数的局部变量
+# 也不是全局变量
+# 而是来自外层函数作用
+
+def outer():
+    x = 10
+
+    def inner():
+        # x 来自 outer，不是 inner 自己的局部变量
+        nonlocal x # nonlocal 表示x不是局部变量，而且外层变量
+        x += 1 # 这里修改了x的值，每次都对自由变量x进行了 +1 操作
+        return x
+
+    print(f"x的值：{x}")  # x的值：10
+    return inner
+
+
+inn = outer()
+result1 = inn()
+result2 = inn()
+result3 = inn()
+result4 = inn()
+print(f"inner执行结果：{result1}") # inner执行结果：11
+print(f"inner执行结果：{result2}") # inner执行结果：12
+print(f"inner执行结果：{result3}") # inner执行结果：13
+print(f"inner执行结果：{result4}") # inner执行结果：14
--- day16_functional/README.md
+++ day16_functional/README.md
@@ -1,4 +1,12 @@
 # Python 函数式编程
 
-- day16_functional/01_function_partial.py : 偏函数
-- day16_functional/02_function_decorator.py # 装饰器函数实现自定义注解,给方法添加打印日志功能
+- day16_functional/01_function_partial.py
+  - 偏函数
+- day16_functional/02_function_decorator.py
+  - 装饰器函数实现自定义注解,给方法添加打印日志功能
+- day16_functional/03_function_closure_1.py 
+  - 闭包与自由变量的基本使用
+- day16_functional/03_function_closure_2.py
+  - 闭包与自由变量，使用nonlocal，让内部函数记住自由变量，并修改自由变量，实现参数自增。
+- day16_functional/03_function_closure_3.py
+  - 闭包与自由变量的坑及解决办法
