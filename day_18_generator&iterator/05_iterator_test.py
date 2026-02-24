b59f8b8da07e4badcf9be605a42e4b75d055f9b9
George <george_95@126.com>
2026-02-24 19:27:21 +0800

    迭代器的简单测试

--- day_18_generator&iterator/05_iterator_test.py
+++ day_18_generator&iterator/05_iterator_test.py
@@ -23,3 +23,9 @@ print(c) # False
 
 print(type("")) # <class 'str'>
 print(isinstance("", Iterator)) # False
+
+# 将列表、字典、字符串转换成迭代器, 使用 iter() 方法
+print("***********************")
+print(isinstance(iter([]), Iterator)) # True
+print(isinstance(iter("123"), Iterator)) # True
+print(isinstance(iter({'name': 'George'}), Iterator)) # True
